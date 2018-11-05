082cbdf86a66b60f1d3770189bfc4bcf74ba3be3
Tomasz Jarych <tjarych@gmail.com>
2018-11-05 13:31:56 +0100

    Equality Test
    Signed-off-by: Tomasz Jarych <tjarych@gmail.com>

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,16 +1,22 @@
 package guru.springframework;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class Dollar {
 
 	private int amount;
 
 	Dollar times(int multiplier) {
-
-		return new Dollar(amount*multiplier);
+		return new Dollar(amount * multiplier);
 	}
+
+
+	
+	
+	
 }
